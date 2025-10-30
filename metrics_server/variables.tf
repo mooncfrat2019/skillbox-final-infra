6a6baa16fc58daee33d5053f091ffaf3b7404d3c
Igor Kolomoytsev <i.kolomoytsev@vk.team>
2025-10-30 21:43:08 +0300

    add metrics deploy files

---
+++ metrics_server/variables.tf
@@ -0,0 +1,43 @@
+
+variable "username" {
+  description = "VK Cloud username"
+  type        = string
+  sensitive   = true
+}
+
+variable "password" {
+  description = "VK Cloud password"
+  type        = string
+  sensitive   = true
+}
+
+variable "project_id" {
+  description = "VK Cloud project ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "compute_flavor" {
+  type = string
+  default = "STD3-2-4"
+}
+variable "key_pair_name" {
+  type = string
+  default = "id_rsa_windows"
+}
+variable "availability_zone_name" {
+  type = string
+  default = "MS1"
+}
+
+variable "instance_name" {
+  description = "Base name of instance"
+  type        = string
+  default     = "terraform-vm"
+}
+
+variable "external_network_name" {
+  description = "Name of the external network"
+  type        = string
+  default     = "ext-net"
+}
